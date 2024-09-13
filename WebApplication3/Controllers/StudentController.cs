a9ee04ae374e4035aa68d3e93305014cfec51fbe
HananHIssa <hananissa2018@gmail.com>
2024-09-13 16:37:05 +0300

    final

---
+++ WebApplication3/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult getAll()
+        {
+            return View();
+        }
+        public IActionResult create()
+        {
+            return View();
+        }
+        public IActionResult update()
+        {
+            return View();
+        }
+        public IActionResult delete()
+        {
+            return View();
+        }
+    }
+}
